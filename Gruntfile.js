5a39b2db4c3b5df74398dfa5c1319726281ddcbb
jcenturion <javier.centurion@auth0.com>
2016-08-24 14:46:28 -0700

    Setting up repo

---
+++ Gruntfile.js
@@ -0,0 +1,91 @@
+const fs   = require('fs');
+const path = require('path');
+
+module.exports = function exec(grunt) {
+  const TMPL_PATH = 'webtask-editor/assets';
+
+  const pkg  = require('./package');
+
+  const minor = pkg.version.replace(/\.(\d)*$/, '');
+  const major = pkg.version.replace(/\.(\d)*\.(\d)*$/, '');
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    aws_s3: {
+      options: {
+        accessKeyId:     process.env.S3_KEY,
+        secretAccessKey: process.env.S3_SECRET,
+        bucket:          process.env.S3_BUCKET,
+        region:          process.env.S3_REGION,
+        uploadConcurrency: 5,
+        params: {
+          CacheControl: 'public, max-age=300'
+        },
+      },
+      clean: {
+        files: [
+          { action: 'delete', dest: `${TMPL_PATH}/templates.json` }
+        ]
+      },
+      publish: {
+        files: [
+          {
+            expand: true,
+            cwd: 'release/',
+            src: ['templates.json'],
+            dest: `${TMPL_PATH}`
+          }
+        ]
+      },
+      clean_dev: {
+        files: [
+          { action: 'delete', dest: `${TMPL_PATH}/templates.json` }
+        ]
+      },
+      publish_dev: {
+        files: [
+          {
+            expand: true,
+            cwd: 'release/',
+            src: ['templates.json'],
+            dest: `${TMPL_PATH}`
+          }
+        ]
+      },
+    },
+    http: {
+      purge_templates: {
+        options: {
+          url: `${process.env.CDN_ROOT}/${TMPL_PATH}/templates.json`,
+          method: 'DELETE'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-aws-s3');
+  grunt.loadNpmTasks('grunt-http');
+
+  grunt.registerTask('templates', () => {
+    const templates = [];
+
+    fs.readdirSync(path.join('templates')).filter((file) => {
+      return file.indexOf('.yaml') >= 0;
+    }).map((tmpl) => {
+      const template = grunt.file.readYAML(path.join('templates', tmpl));
+      templates.push(template);
+    });
+
+    grunt.file.write(path.join('release', 'templates.json'), JSON.stringify(templates, null, 2));
+  });
+
+  grunt.registerTask('purge-cdn', ['http:purge_templates']);
+
+  grunt.registerTask('cdn', [
+    'templates',
+    'aws_s3:clean',
+    'aws_s3:publish',
+    'purge-cdn'
+  ]);
+};
